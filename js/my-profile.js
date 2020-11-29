0a03560bfaa5edeb716037d007d04807c91a9fc0
tintin-18 <espinosaagustin18@gmail.com>
2020-11-29 16:36:50 -0300

    Entregable 07

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,6 +1,28 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function (e) {
+function guardar(){
+let nombre = document.getElementById("inputName").value;
+let apellido = document.getElementById("inputLastname").value;
+let email = document.getElementById("inputEmail4").value;
+let edad = document.getElementById("inputAge").value;
+let telefono = document.getElementById("inputPhone").value;
 
-});
+if (nombre === "" ||apellido === "" ||email === "" ||edad === "" ||telefono === ""){
+    alert ("Completar los campos");
+    return false;}
+
+let datos={
+    name: nombre,
+    lastname: apellido,
+    mail: email,
+    telephone: telefono,
+    age: edad}
+localStorage.setItem("data",JSON.stringify(datos))}
+
+
+    if(localStorage.getItem("data")!= null){
+guardarDatos=JSON.parse(localStorage.getItem("data"))
+document.getElementById("inputName").value=guardarDatos.name;
+document.getElementById("inputLastname").value=guardarDatos.lastname;
+document.getElementById("inputEmail4").value=guardarDatos.mail;
+document.getElementById("inputAge").value=guardarDatos.telephone;
+document.getElementById("inputPhone").value=guardarDatos.age;
+    }
